c6bcce6890d9beb77cb9bd3c61857c5a935f5605
pengzhe <18900433@qq.com>
2025-12-07 21:01:04 +0800

    chat client

--- chat-client/src/main/java/com/pengzhe/tech/ChatClientController.java
+++ chat-client/src/main/java/com/pengzhe/tech/ChatClientController.java
@@ -11,22 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
  * https://java2ai.com/docs/1.0.0.2/tutorials/basics/chat-client/?spm=5176.29160081.0.0.2856aa5cmUTyXC#%E5%88%9B%E5%BB%BA-chatclient
  */
 @RestController
-public class ChatClientController
-{
+public class ChatClientController {
     private final ChatClient dashScopeChatClient;
 
-    /** zzyybs@126.com
+    /**
      * ChatClient不支持自动输入，依赖ChatModel对象接口，ChatClient.builder(dashScopeChatModel).build();
+     *
      * @param dashScopeChatModel
      */
-    public ChatClientController(ChatModel dashScopeChatModel)
-    {
+    public ChatClientController(ChatModel dashScopeChatModel) {
         this.dashScopeChatClient = ChatClient.builder(dashScopeChatModel).build();
     }
 
     @GetMapping("/chatclient/dochat")
-    public String doChat(@RequestParam(name = "msg",defaultValue = "2加9等于几") String msg)
-    {
+    public String doChat(@RequestParam(name = "msg", defaultValue = "2加9等于几") String msg) {
         return dashScopeChatClient.prompt().user(msg).call().content();
     }
 }
