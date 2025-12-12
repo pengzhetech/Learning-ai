aed4fd1a9dd430559425b5661f33c7507394d8d8
pengzhe <18900433@qq.com>
2025-12-12 21:17:48 +0800

    persistent

---
+++ persistent/src/main/java/com/pengzhe/tech/ChatMemory4RedisController.java
@@ -0,0 +1,47 @@
+package com.pengzhe.tech;
+
+import jakarta.annotation.Resource;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.ai.chat.memory.ChatMemory.CONVERSATION_ID;
+
+/**
+ * @auther zzyybs@126.com
+ * @create 2025-07-28 18:40
+ * @Description TODO
+ */
+@RestController
+public class ChatMemory4RedisController
+{
+    @Resource(name = "qwenChatClient")
+    private ChatClient qwenChatClient;
+
+    /**
+     * zzyybs@126.com
+     * @param msg
+     * @param userId
+     * @return
+     */
+    @GetMapping("/chatmemory/chat")
+    public String chat(String msg, String userId)
+    {
+        /*return qwenChatClient.prompt(msg).advisors(new Consumer<ChatClient.AdvisorSpec>()
+        {
+            @Override
+            public void accept(ChatClient.AdvisorSpec advisorSpec)
+            {
+                advisorSpec.param(CONVERSATION_ID, userId);
+            }
+        }).call().content();*/
+
+
+        return qwenChatClient
+                .prompt(msg)
+                .advisors(advisorSpec -> advisorSpec.param(CONVERSATION_ID, userId))
+                .call()
+                .content();
+
+    }
+}
