c8dd606d1ddc60d081efc41a4015038f0feb0212
pengzhe <18900433@qq.com>
2025-12-12 20:25:19 +0800

    prompt

---
+++ prompt/src/main/java/com/pengzhe/tech/Saa05PromptApplication.java
@@ -0,0 +1,17 @@
+package com.pengzhe.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @create 2025-07-25 20:56
+ * @Description 知识出处，https://java2ai.com/docs/1.0.0.2/tutorials/basics/prompt/?spm=5176.29160081.0.0.2856aa5cdeol7a
+ */
+@SpringBootApplication
+public class Saa05PromptApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(Saa05PromptApplication.class,args);
+    }
+}
